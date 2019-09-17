a61f5c28f65bf92b1dd33554f62390ff85041cce
marcos <eptaccio@gmail.com>
2019-09-17 01:05:40 -0300

    Start crawler

--- crawler/index.js
+++ crawler/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const { Character } = require('../database/models')
+const axios = require('axios')
+
+const start = async () => {
+  console.log('hello world')
+}
+
+start()
+  .then(_ => process.exit())
+  .catch(_ => process.exit(1))
