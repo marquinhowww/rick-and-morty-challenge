9c1bb8aa41e8c4688961caa4761b8118305a38a0
marcos <eptaccio@gmail.com>
2019-09-17 02:15:59 -0300

    Isolate load function

---
+++ crawler/dataLoader.js
@@ -0,0 +1,27 @@
+const { config } = require('../config')
+const { models } = require('../database')
+const { formatData } = require('./formatData')
+const { logger, http } = require('../services')
+
+const { Character } = models
+
+const load = async ({ curretURL = config.API_URL } = {}) => {
+  logger.info('Current URL', curretURL)
+
+  const { data } = await http.get(curretURL)
+
+  const formatedData = data.results.map(formatData)
+
+  await Character.insertMany(formatedData)
+
+  const nextURL = data.info && data.info.next
+  if (nextURL) {
+    return load({ curretURL: nextURL })
+  }
+
+  logger.info('Load completed')
+}
+
+module.exports = {
+  load
+}
--- crawler/index.js
+++ crawler/index.js
@@ -1,26 +1,6 @@
-const { config } = require('../config')
-const { connect, models } = require('../database')
-const { formatData } = require('./formatData')
-const { logger, http } = require('../services')
-
-const { Character } = models
-
-const load = async ({ curretURL = config.API_URL } = {}) => {
-  logger.info('Current URL', curretURL)
-
-  const { data } = await http.get(curretURL)
-
-  const formatedData = data.results.map(formatData)
-
-  await Character.insertMany(formatedData)
-
-  const nextURL = data.info && data.info.next
-  if (nextURL) {
-    return load({ curretURL: nextURL })
-  }
-
-  logger.info('Load completed')
-}
+const { connect } = require('../database')
+const { load } = require('./dataLoader')
+const { logger } = require('../services')
 
 connect()
   .then(load)
