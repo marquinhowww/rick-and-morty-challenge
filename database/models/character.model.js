f125d1b112679bc9e14f13257ec91bffac258673
marcos <eptaccio@gmail.com>
2019-09-17 00:59:09 -0300

    Add Character model

---
+++ database/models/character.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const CharacterSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+})
+
+const Character = mongoose.model('Character', CharacterSchema)
+
+module.exports = {
+  Character
+}
