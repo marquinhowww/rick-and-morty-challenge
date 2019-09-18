a1f61af9289c11e9a56dbb63a93574dff8e3317d
marcos <eptaccio@gmail.com>
2019-09-18 15:00:46 -0300

    Start list query

--- api/controllers/character/list.js
+++ api/controllers/character/list.js
@@ -1,5 +1,35 @@
-const list = (req, res, next) => {
-  res.send({ hello: 'world' })
+const { models } = require('../../../database')
+
+const { Character } = models
+
+const list = async (req, res, next) => {
+  const characters = await Character.aggregate([
+    {
+      $group: {
+        _id: '$name',
+        dimensions_count: { $sum: 1 },
+        image: {
+          $last: '$image'
+        }
+      }
+    },
+    {
+      $sort: {
+        dimensions_count: -1
+      }
+    },
+    {
+      $project: {
+        image: '$image',
+        name: '$_id',
+        dimensions_count: '$dimensions_count',
+        _id: 0
+      }
+    }
+  ])
+
+  res.send({ characters })
+
   next()
 }
 
