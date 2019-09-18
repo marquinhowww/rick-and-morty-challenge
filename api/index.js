f1cc8131377ed354139db47b24e698b1c3464cb9
marcos <eptaccio@gmail.com>
2019-09-18 20:25:45 -0300

    Add database connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const { config } = require('../config')
 const restify = require('restify')
 const { logger } = require('../services')
+const { connect } = require('../database')
+
+connect()
 
 const { routes: loadCharacterRoutes } = require('./controllers/character/routes')
 
